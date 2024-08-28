cb11a9674905d9d07a37d2bfc8637dd3289962cd
Shriya_HW <shriyadubey220@gmail.com>
2024-08-28 07:31:13 +0530

    27 august

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+class FirstProgram
+{
+	public static void main(String args[])
+	{
+		System.out.print("Shriya Dubey");
+	}
+}
+// Comments
+/* for executing
+javac filename.java
+java classname */
