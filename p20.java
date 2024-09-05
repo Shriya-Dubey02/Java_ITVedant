48c88ec6efce01aae53d1ca8d5109f544b963167
Shriya_HW <shriyadubey220@gmail.com>
2024-09-05 10:56:07 +0530

    5 September

---
+++ p20.java
@@ -0,0 +1,23 @@
+class p20{
+	public static void main(String args[])
+	{
+		// Switch statements
+		switch(10)
+		{
+			case 1:
+			System.out.println("Case 1");
+			break;
+			
+			case 2:
+			System.out.println("Case 2");
+			break;
+			
+			case 3:
+			System.out.println("Case 3");
+			break;
+			
+			default:
+			System.out.println("No matching case");
+		}
+	}
+}
