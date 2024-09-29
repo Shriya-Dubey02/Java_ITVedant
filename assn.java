a9be63c2305e9f1a965bf6f06ca21d652096f844
ShriyaDubey22 <shriyadubey220@gmail.com>
2024-09-29 19:36:09 +0530

    Add files via upload

---
+++ assn.java
@@ -0,0 +1,12 @@
+class assn
+{
+	public static void main(String args[])
+	{
+		int id=101;
+		String name="Shriya";
+		long mobile_no= 7058315;
+		double marks= 95.80;
+
+		System.out.println(id + " "+ name + " " + mobile_no + " " + marks);
+	}
+}
